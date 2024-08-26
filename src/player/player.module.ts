b40de14c699330cd9af7c3d363ee99545443b09c
Sandi-san <nicksrebotsandi@gmail.com>
2024-08-26 20:12:09 +0200

    added missing import

--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PlayerService } from './player.service';
 import { PlayerController } from './player.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CardModule } from 'src/card/card.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CardModule],
   providers: [PlayerService],
   controllers: [PlayerController],
   exports: [PlayerService],
